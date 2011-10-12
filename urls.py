e8b3e95f114302671a57e27003fefa7e9388f322
cihann <apple@APPLEs-MacBook-Air.local>
2011-10-13 00:59:29 +0300

    fixed requirements

--- urls.py
+++ urls.py
@@ -7,9 +7,5 @@ urlpatterns = patterns('',
     url(r'^$', 'djangoistanbul.views.home', name='home'),
     url(r'^blog/', include('blog.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
