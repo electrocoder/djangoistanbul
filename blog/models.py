65559dd98d001ae7d48fa0f6cc2de6da3b3e5bf4
Cihan Okyay <okyaycihan@gmail.com>
2011-10-22 13:48:14 +0300

    tinymce

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.db.models import permalink
+from django.contrib.auth.models import User
 
 class Blog(models.Model):
     title = models.CharField(max_length=100, unique=True)
     slug = models.SlugField(max_length=100, unique=True)
     body = models.TextField()
     posted = models.DateField(db_index=True, auto_now_add=True)
+    author = models.ForeignKey(User)
     category = models.ForeignKey('blog.Category')
 
     def __unicode__(self):
